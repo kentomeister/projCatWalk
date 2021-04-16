f6f4e2c5143e504cd2ad1ec8b290ee7649810967
Alexander Zinn <alexzinnde@gmail.com>
2021-04-16 14:02:53 -0400

    Complete MVP of Rating Stars component

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,15 @@
-/* eslint-disable import/extensions */
 import React from 'react';
-import StarRating from './components/shared/StarRating.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {};
-    this.handleRatingClick = this.handleRatingClick.bind(this);
-  }
-
-  handleRatingClick(rating) {
-   console.log(rating);
   }
 
   render() {
     return (
       <div>
-        <StarRating
-          rating="2.5"
-          size="20"
-          handleRatingClick={this.handleRatingClick}
-        />
+        <h1>This will be the front end!</h1>
       </div>
     );
   }
--- src/components/shared/StarRating.jsx
+++ src/components/shared/StarRating.jsx
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from 'react-icons/io';
 
-const StarRating = ({ rating, size, handleRatingClick }) => {
+/**
+ *
+ * @param {string} rating number 0.0 - 5.0 representing number of stars to be yellow. 0.5 resolution
+ *
+ * @param {string} size how large the icons should be. Default: 20
+ *
+ * @param {bool} isClickable enable/disable user clicking, Default: true
+ *
+ * @param {func} handleRatingClick handle the user clicking on a star.
+ * Passes a string with the rating of the clicked star to function
+ *
+ * @returns {StarRating} 5 stars displayed on the screen which can be clicked on.
+ */
+
+const StarRating = ({
+  rating, size, isClickable, handleRatingClick,
+}) => {
   const [whole, part] = rating.split('.');
   let starArr = [...Array(5)];
   starArr = starArr.map((p, i) => {
@@ -23,11 +40,11 @@ const StarRating = ({ rating, size, handleRatingClick }) => {
         if (star === 1) {
           return (
             <IoIosStar
-              size={hover === ratingValue ? size * 1.5 : size}
+              size={hover === ratingValue ? size * 1.2 : size}
               key={ratingValue}
               data-rating={ratingValue}
-              onClick={() => { handleRatingClick(ratingValue); }}
-              onMouseOver={() => setHover(ratingValue)}
+              onClick={() => isClickable && handleRatingClick(ratingValue.toString())}
+              onMouseOver={() => isClickable && setHover(ratingValue)}
               onMouseLeave={() => setHover(null)}
               className="star"
             />
@@ -36,28 +53,41 @@ const StarRating = ({ rating, size, handleRatingClick }) => {
         if (star === 0.5) {
           return (
             <IoIosStarHalf
-              size={hover === ratingValue ? size * 1.5 : size}
+              size={hover === ratingValue ? size * 1.2 : size}
               key={ratingValue}
               className="star"
-              onClick={() => { handleRatingClick(ratingValue); }}
-              onMouseOver={() => setHover(ratingValue)}
+              onClick={() => isClickable && handleRatingClick(ratingValue.toString())}
+              onMouseOver={() => isClickable && setHover(ratingValue)}
               onMouseLeave={() => setHover(null)}
             />
           );
         }
         return (
           <IoIosStarOutline
-            size={hover === ratingValue ? size * 1.5 : size}
+            size={hover === ratingValue ? size * 1.2 : size}
             key={ratingValue}
             className="star dark"
-            onClick={() => { handleRatingClick(ratingValue); }}
-            onMouseOver={() => setHover(ratingValue)}
+            onClick={() => isClickable && handleRatingClick(ratingValue.toString())}
+            onMouseOver={() => isClickable && setHover(ratingValue)}
             onMouseLeave={() => setHover(null)}
           />
         );
       })}
     </div>
   );
+};
+
+StarRating.propTypes = {
+  rating: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  isClickable: PropTypes.bool,
+  handleRatingClick: PropTypes.func.isRequired,
+
+};
+
+StarRating.defaultProps = {
+  size: '20',
+  isClickable: true,
 };
 
 export default StarRating;
