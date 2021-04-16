b1c887352b9f9e1dc1c6123443242fe11afa7243
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-16 14:31:39 -0400

    Rename ratings-review file to review.jsx, make helpful-counter subcomponent file, make review-list subcomponent file.

---
+++ src/components/ratings-review/review-list.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ReviewList extends React.Components {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>This is my review list component</div>
+    );
+  }
+}
+
+export default ReviewList;
