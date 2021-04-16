e74c3115cee7b79f384e417951ac4c4ba3960321
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-16 15:22:43 -0400

    Build structure to my main component. Rename the file name to ratings-reviews.

---
+++ src/components/ratings-review/ratings-reviews.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class RatingsReviews extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="ratings-reviews">
+          This my whole ratings review component - Tomas
+          <div>Ratings Breakdown</div>
+          <div>Sort button</div>
+          <div>Review list</div>
+          <div>Product Breakdown</div>
+          <div>Write a review</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RatingsReviews;
