c2b38c6374f8809e80f2123bdcf75f854d0709a7
Alexander Zinn <alexzinnde@gmail.com>
2021-04-17 13:35:26 -0400

    Add morgan logging to server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const api = require('./apiCalls.js');
 const productOverview = require('./productOverviewHelpers.js');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+if (process.env.NODE_ENV === 'production') {
+  console.log('in production');
+  app.use(morgan('tiny'));
+}
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
