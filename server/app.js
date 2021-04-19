0c7c73833b8de0ea7fd37b4aae47ee4530a019dd
Alexander Zinn <alexzinnde@gmail.com>
2021-04-19 12:28:17 -0400

    Split server.js into two files to allow for testing with supertest npm package

---
+++ server/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const api = require('./apiCalls');
+
+const productOverview = require('./productOverviewHelpers.js');
+
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('tiny'));
+}
+
+app.get('/productOverview/:productId', (req, res) => {
+  const { productId } = req.params;
+  Promise.all([
+    api.getProductInfo(productId),
+    api.getProductStyles(productId),
+    api.getProductReviewMeta(productId),
+    api.getProductReviews(productId),
+  ])
+    .then((results) => results.map((result) => result.data))
+    .then((rawData) => productOverview.parseData(rawData))
+    .then((parsedData) => res.send(parsedData).end())
+    .catch((err) => res.status(500).send(err));
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,34 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const api = require('./apiCalls.js');
-const productOverview = require('./productOverviewHelpers.js');
-
-const app = express();
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-if (process.env.NODE_ENV === 'production') {
-  app.use(morgan('tiny'));
-}
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'public')));
-
-app.get('/productOverview/:productId', (req, res) => {
-  const { productId } = req.params;
-  Promise.all([
-    api.getProductInfo(productId),
-    api.getProductStyles(productId),
-    api.getProductReviewMeta(productId),
-    api.getProductReviews(productId),
-  ])
-    .then((results) => results.map((result) => result.data))
-    .then((rawData) => productOverview.parseData(rawData))
-    .then((parsedData) => res.send(parsedData).end())
-    .catch((err) => res.status(500).send(err));
-});
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
