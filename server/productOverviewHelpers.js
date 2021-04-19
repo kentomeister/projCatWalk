cf3ddc89e709b3a7d56900121f67875586cc812e
Alexander Zinn <alexzinnde@gmail.com>
2021-04-19 12:04:23 -0400

    Implement tests for productOverviewHelpers.js

--- server/productOverviewHelpers.js
+++ server/productOverviewHelpers.js
@@ -18,13 +18,13 @@ const calcAvgRating = (ratingsObj) => {
 const parseData = (rawData) => {
   const result = {
     id: rawData[0].id,
-    category: rawData[0].category || 'not specified',
-    default_price: rawData[0].default_price || 'not specified',
-    slogan: rawData[0].slogan || 'not specified',
-    description: rawData[0].description || 'not specified',
+    category: rawData[0].category || null,
+    default_price: rawData[0].default_price || null,
+    slogan: rawData[0].slogan || null,
+    description: rawData[0].description || null,
     avgRating: calcAvgRating(rawData[2].ratings) || -1,
     numReviews: rawData[3].results.length || 0,
-    styles: rawData[1].results || [],
+    styles: rawData[1].results || null,
   };
 
   return result;
@@ -32,24 +32,3 @@ const parseData = (rawData) => {
 
 module.exports.parseData = parseData;
 module.exports.calcAvgRating = calcAvgRating;
-
-/**
-result = {
-  // This data comes from rawData[0]
-  id: '1909',
-  category: 'Accessories',
-  default_price: '69.00',
-  slogan: 'You\'ve got to wear shades',
-  description: 'There will be a description with a bunch of text here',
-
-  //this data comes from rawData[1]
-  styles: 'rawData[1].results'
-
-  // This data comes from rawData[2]
-  avgRating: 'calcualte the average rating from rawData[2]',
-
-  // this data comes from rawData[3]
-  numReviews: 'rawData[3].results.length'
-
-}
- */
