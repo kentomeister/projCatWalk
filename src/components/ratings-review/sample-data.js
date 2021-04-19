a3365acc5877c3ca374da2b7d0e7c0db06b3ae6b
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-19 15:15:57 -0400

    Make a sample data file to test review component

---
+++ src/components/ratings-review/sample-data.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/extensions */
+const data = {
+  product: '19090',
+  page: 0,
+  count: 5,
+  results: [
+    {
+      review_id: 228392,
+      rating: 4,
+      summary: 'I am liking these glasses',
+      recommend: true,
+      response: "Glad you're enjoying the product!",
+      body: "They are very dark. But that's good because I'm in very sunny spots",
+      date: '2019-06-23T00:00:00.000Z',
+      reviewer_name: 'bigbrotherbenjamin',
+      helpfulness: 7,
+      photos: [],
+    },
+    {
+      review_id: 308769,
+      rating: 5,
+      summary: 'this was awesome',
+      recommend: true,
+      response: null,
+      body: 'like I said, awesome',
+      date: '2021-03-19T00:00:00.000Z',
+      reviewer_name: 'fchopin2',
+      helpfulness: 0,
+      photos: [],
+    },
+    {
+      review_id: 228395,
+      rating: 5,
+      summary: "I'm not a fan!",
+      recommend: false,
+      response: "Sorry to hear. Is there anything in particular you don't like?",
+      body: "I don't like them",
+      date: '2019-06-16T00:00:00.000Z',
+      reviewer_name: 'negativity',
+      helpfulness: 0,
+      photos: [],
+    },
+  ],
+};
+
+export default data;
