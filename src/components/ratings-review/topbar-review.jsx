ec1ce8edce70167271c6217b87da98edaea1bfb4
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-19 15:21:56 -0400

    Add sample data and formatted given date to human readable format

--- src/components/ratings-review/topbar-review.jsx
+++ src/components/ratings-review/topbar-review.jsx
@@ -1,11 +1,18 @@
+/* eslint-disable import/extensions */
 import React from 'react';
+import moment from 'moment';
+import data from './sample-data.js';
+
+const { date } = data.results[0].date;
+const formattedDate = moment(date).format('LL');
+console.log(formattedDate);
 
 function Topbar() {
   return (
     <div className="topbar-cont">
       <div className="review-stars">stars</div>
-      <div className="review-username">username</div>
-      <div className="review-date">date of review</div>
+      <div className="review-username">{data.results[0].reviewer_name}</div>
+      <div className="review-date">{formattedDate}</div>
     </div>
   );
 }
