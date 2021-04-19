25bb7ea5c1a7aa146c0d5dd3a8cb898ccccb987f
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-19 15:21:21 -0400

    Add sample data and conditional rendering for seller-response

--- src/components/ratings-review/review.jsx
+++ src/components/ratings-review/review.jsx
@@ -2,22 +2,31 @@
 import React from 'react';
 import Helpful from './helpful-counter.jsx';
 import Topbar from './topbar-review.jsx';
+import data from './sample-data.js';
 
 class Reviews extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
+  // we map over all the objects to render the reviews
 
   render() {
     return (
       <li className="review-cont">
         <div>Ratings & Reviews</div>
         <Topbar />
-        <div className="review-summary">Review Summary/Title</div>
-        <div className="review-body">Review Body</div>
-        <div className="recommended-tag">**I recommend this product text**</div>
-        <div className="seller-response">**Response from seller**</div>
+        <div className="review-summary">{data.results[0].summary}</div>
+        <div className="review-body">{data.results[0].body}</div>
+        <div className="recommended-tag">
+          **I recommend this product:
+          {data.results[0].recommend.toString()}
+        </div>
+        <div>
+          {data.results[0].response
+            ? <div className="seller-response">{data.results[0].response}</div>
+            : <div>{}</div>}
+        </div>
         <div>
           <Helpful />
         </div>
