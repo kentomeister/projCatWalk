b029d80c5fecc0226facfd862f40cd9135a22a8b
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-20 15:11:58 -0400

    Implement componentDidMount to set state with review data from API call

--- src/components/ratings-review/ratings-reviews.jsx
+++ src/components/ratings-review/ratings-reviews.jsx
@@ -1,21 +1,31 @@
 /* eslint-disable import/extensions */
 import React from 'react';
+import axios from 'axios';
 import ReviewList from './review-list.jsx';
 
 class RatingsReviews extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      productReviews: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/reviews/19092')
+      .then((res) => this.setState({ productReviews: res.data.results }))
+      .catch((err) => console.log('err: ', err));
   }
 
   render() {
+    const { productReviews } = this.state;
     return (
       <div>
         <div className="ratings-reviews">
           This my whole ratings review component - Tomas
           <div>Ratings Breakdown</div>
           <div>Sort button</div>
-          <ReviewList />
+          <ReviewList reviews={productReviews} />
           <div>Product Breakdown</div>
           <div>Write a review</div>
         </div>
