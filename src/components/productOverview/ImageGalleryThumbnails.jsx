e27fa0b1f20fb3d86619f5f68099318d581c1143
Alexander Zinn <alexzinnde@gmail.com>
2021-04-21 13:23:20 -0400

    Add arrows to image gallery thumbnails basic functionality

--- src/components/productOverview/ImageGalleryThumbnails.jsx
+++ src/components/productOverview/ImageGalleryThumbnails.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+/* eslint-disable import/extensions */
+import React, { useState } from 'react';
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/Ai';
 import ImageThumbnail from './ImageThumbnail.jsx';
 
-const ImageGalleryThumbnails = ({ images, currentImage, handleImageThumbnailclick }) => (
-  <div className="image-gallery-thumbnail-container">
-    {images.map((image, i) => (
-      <ImageThumbnail
-        imageUrl={image.url}
-        selected={currentImage === image.url}
-        key={image.url}
-        handleImageThumbnailclick={handleImageThumbnailclick}
+const ImageGalleryThumbnails = ({ images, currentImage, handleImageThumbnailclick }) => {
+  const [startPointer, setStartPointer] = useState(0);
+  const imagesToDisplay = images.slice(startPointer, startPointer + 4);
+  const handleArrowDownClick = () => {
+    if (startPointer < images.length - 4) {
+      setStartPointer((prevStartPointer) => prevStartPointer + 1);
+    }
+  };
+  const handleArrowUpClick = () => {
+    if (startPointer > 0) {
+      setStartPointer((prevStartPointer) => prevStartPointer - 1);
+    }
+  };
+  return (
+    <div className="image-gallery-thumbnail-container">
+      <AiOutlineArrowUp
+        size="75"
+        color="white"
+        cursor="pointer"
+        onClick={handleArrowUpClick}
       />
-    ))}
-  </div>
-);
+      {imagesToDisplay.map((image) => (
+        <ImageThumbnail
+          imageUrl={image.url}
+          selected={currentImage === image.url}
+          key={image.url}
+          handleImageThumbnailclick={handleImageThumbnailclick}
+        />
+      ))}
+      <AiOutlineArrowDown
+        size="75"
+        color="white"
+        cursor="pointer"
+        onClick={handleArrowDownClick}
+      />
+    </div>
+  );
+};
 
 export default ImageGalleryThumbnails;
