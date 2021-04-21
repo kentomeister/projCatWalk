259039ba0333dc602ad63cc7597f861675e0169f
Alexander Zinn <alexzinnde@gmail.com>
2021-04-21 14:39:51 -0400

    Left and right arrows fully functioning

--- src/components/productOverview/ImageGallery.jsx
+++ src/components/productOverview/ImageGallery.jsx
@@ -6,37 +6,47 @@ import ImageGalleryThumbnails from './ImageGalleryThumbnails.jsx';
 const ImageGallery = ({ images }) => {
   if (images === undefined) return '';
   const [imageIndex, setImageIndex] = useState(0);
-  const [currentImageUrl, setCurrentImageUrl] = useState(images[0].url);
+  const [currentImageUrl, setCurrentImageUrl] = useState(images[imageIndex].url);
   useEffect(() => { setCurrentImageUrl(images[0].url); }, [images]);
+  useEffect(() => { setCurrentImageUrl(images[imageIndex].url); }, [imageIndex]);
 
   const handleImageThumbnailclick = (imageUrl) => setCurrentImageUrl(imageUrl);
-  const handleLeftArrowClick = () => console.log('left click');
-  const handleRightArrowClick = () => console.log('Right Click');
+  const handleLeftArrowClick = () => setImageIndex((prevIndex) => prevIndex - 1);
+  const handleRightArrowClick = () => setImageIndex((prevIndex) => prevIndex + 1);
+  const showLeftArrow = imageIndex > 0;
+  const showRightArrow = imageIndex < images.length - 1;
+
   return (
 
     <div
       className="image-gallery"
       style={{ backgroundImage: `url(${currentImageUrl})` }}
     >
-      <AiOutlineArrowLeft
-        color="white"
-        size="40"
-        className="image-gallery-left-arrow"
-        cursor="pointer"
-        onClick={handleLeftArrowClick}
-      />
+      {
+        showLeftArrow
+        && <AiOutlineArrowLeft
+          color="white"
+          size="40"
+          className="image-gallery-left-arrow"
+          cursor="pointer"
+          onClick={handleLeftArrowClick}
+        />
+      }
       <ImageGalleryThumbnails
         images={images}
         currentImage={currentImageUrl}
         handleImageThumbnailclick={handleImageThumbnailclick}
       />
-      <AiOutlineArrowRight
-        color="white"
-        size="40"
-        className="image-gallery-right-arrow"
-        cursor="pointer"
-        onClick={handleRightArrowClick}
-      />
+      {
+        showRightArrow
+        && <AiOutlineArrowRight
+          color="white"
+          size="40"
+          className="image-gallery-right-arrow"
+          cursor="pointer"
+          onClick={handleRightArrowClick}
+        />
+      }
     </div>
   );
 };
