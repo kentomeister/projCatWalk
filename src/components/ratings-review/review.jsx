6a0ab3538c39a824baa453e2ecd1c5bdcab1184d
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-23 13:45:57 -0400

    Add star review feature, helpful clicks only once, recommended and seller response render only if true and if there's a response.

--- src/components/ratings-review/review.jsx
+++ src/components/ratings-review/review.jsx
@@ -2,44 +2,62 @@
 import React from 'react';
 import Helpful from './helpful-counter.jsx';
 import Topbar from './topbar-review.jsx';
-import data from './sample-data.js';
 
 class Reviews extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const { helpfulness } = props.review;
     this.state = {
-      helpfulCount: 0,
+      helpfulCount: helpfulness,
+      helpfulClicked: false,
     };
     this.helpfulClick = this.helpfulClick.bind(this);
   }
-  // we map over all the objects to render the reviews
 
   helpfulClick() {
-    this.setState((prevState) => (
-      { helpfulCount: prevState.helpfulCount + 1 }
-    ));
+    const { helpfulClicked } = this.state;
+
+    if (!helpfulClicked) {
+      this.setState((prevState) => (
+        { helpfulCount: prevState.helpfulCount + 1 }
+      ));
+      this.setState(() => (
+        { helpfulClicked: true }
+      ));
+      // axios put request to update click count
+    }
   }
 
   render() {
     const { helpfulCount } = this.state;
+    const {
+      body,
+      date,
+      photos,
+      rating,
+      recommend,
+      response,
+      reviewer_name,
+      summary,
+    } = this.props.review;
     return (
       <li className="review-cont">
         <div>Ratings & Reviews</div>
-        <Topbar />
-        <div className="review-summary">{data.results[0].summary}</div>
-        <div className="review-body">{data.results[0].body}</div>
+        <Topbar stars={rating} name={reviewer_name} date={date} />
+        <div className="review-summary">{summary}</div>
+        <div className="review-body">{body}</div>
         <div>
-          {data.results[0].recommend
+          {recommend
             ? <div className="recommended-tag">**I recommend this product</div>
             : <div>{}</div>}
         </div>
         <div>
-          {data.results[0].response
+          {response
             ? (
               <div className="seller-response">
                 Seller response:
                 {' '}
-                {data.results[0].response}
+                {response}
               </div>
             )
             : <div>{}</div>}
