c7573d5f16805bdb1a045df8f36c701997063dcd
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-27 19:42:04 -0400

    Create the sorting component/s

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'server', 'public'),
+  },
+  module: {
+    rules: [
+      { test: /.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
+  },
+};
