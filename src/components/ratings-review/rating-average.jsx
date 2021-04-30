de8ae940457a794cf9e4628fc37107776cb08e98
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-30 14:27:35 -0400

    Update stars look

--- src/components/ratings-review/rating-average.jsx
+++ src/components/ratings-review/rating-average.jsx
@@ -4,13 +4,13 @@ import starRating from '../shared/starRating.jsx';
 function Average(props) {
   const { average } = props;
   const starAvg = starRating({
-    rating: average.toString(), size: '16', isClickable: false, handleRatingClick: () => 'starrating',
+    rating: average.toString(), size: '25', isClickable: false, handleRatingClick: () => 'starrating',
   });
 
   return (
-    <div>
-      <div className="average">{average}</div>
-      <div className="stars-breakdown">{starAvg}</div>
+    <div className="average-cont">
+      <div className="num-average">{average}</div>
+      <div className="stars-average">{starAvg}</div>
     </div>
   );
 }
