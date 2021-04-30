7a7bfde27c426f3527cca9a62113fdc206cb8433
Tomas Rodriguez <tomasrod09@hotmail.com>
2021-04-30 14:47:32 -0400

    Add star functionality (WIP) and event handlers to update state variables and handle for submit Post.

--- src/components/ratings-review/review-list.jsx
+++ src/components/ratings-review/review-list.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import Reviews from './review.jsx';
 import WriteReview from './write-review.jsx';
 import SortReviews from './sort-reviews.jsx';
+import starRating from '../shared/starRating.jsx';
+
+
+const axios = require('axios');
 
 class ReviewList extends React.Component {
   constructor(props) {
@@ -13,19 +17,28 @@ class ReviewList extends React.Component {
       reviewSum: '',
       reviewBody: '',
       nickname: '',
+      starRating: 1,
+      photos: [],
+      characteristics: {},
+      recommend: false,
+      email: '',
     };
     this.handleClickIncrement = this.handleClickIncrement.bind(this);
     this.handleChangeReviewSum = this.handleChangeReviewSum.bind(this);
     this.handleChangeReviewBody = this.handleChangeReviewBody.bind(this);
     this.handleChangeNickname = this.handleChangeNickname.bind(this);
+    this.handleChangeStarRating = this.handleChangeStarRating.bind(this);
+    this.handleChangePhotos = this.handleChangePhotos.bind(this);
+    this.handleChangeCharacteristics = this.handleChangeCharacteristics.bind(this);
+    this.handleChangeRecommend = this.handleChangeRecommend.bind(this);
     this.handleReviewFormSubmit = this.handleReviewFormSubmit.bind(this);
+    this.getStarsFeature = this.getStarsFeature.bind(this);
   }
 
   handleClickIncrement() {
     this.setState((prevState) => (
       { index: prevState.index + 2 }
     ));
-    console.log(this.state.index);
   }
 
   handleChangeReviewSum(e) {
@@ -46,23 +59,66 @@ class ReviewList extends React.Component {
     });
   }
 
+  handleChangeStarRating(e) {
+    this.setState({
+      starRating: e.target.value,
+    });
+  }
+
+  handleChangePhotos(e) {
+    this.setState({
+      photos: e.target.value,
+    });
+  }
+
+  handleChangeCharacteristics(e) {
+    this.setState({
+      characteristics: e.target.value,
+    });
+  }
+
+  handleChangeRecommend(bool) {
+    this.setState({
+      recommend: bool,
+    });
+  }
+
   handleReviewFormSubmit() {
-    console.log(
-      'summary: ', this.state.reviewSum,
-      ' body: ', this.state.reviewBody,
-      ' nickname: ', this.state.nickname
-    );
+    const { productId } = this.props;
+    let submissionData = {
+      product_id: Number(productId),
+      rating: this.state.starRating,
+      summary: this.state.reviewSum,
+      body: this.state.reviewBody,
+      recommend: this.state.recommend,
+      name: this.state.nickname,
+      email: this.state.email,
+      photos: this.state.photos,
+      characteristics: this.state.characteristics,
+    };
+    axios.post('/submitReviews', (submissionData))
+      .then(() => console.log('success!'))
+      .catch((err) => console.log(err));
+  }
+  getStarsFeature() {
+    return starRating({
+      rating: '0', size: '18', isClickable: true, handleRatingClick: () => 'starrating',
+    });
   }
 
   render() {
     const { reviews } = this.props;
     const { sortSelect } = this.props;
     const { index } = this.state;
+    // const starsRating = starRating({
+    //   rating: '0', size: '18', isClickable: true, handleRatingClick: () => 'starrating',
+    // });
+    console.log(reviews);
+
     return (
       <div className="review-list">
         <div className="sort-btn">
           <SortReviews reviews={reviews} sortSelect={sortSelect} />
-          sort drop down menu?
         </div>
         <ul>
           {reviews.slice(0, index).map((review) => <Reviews review={review} />)}
@@ -70,22 +126,150 @@ class ReviewList extends React.Component {
 
         {/* Modal below */}
 
-        <button type="button" className="loadReviews" onClick={this.handleClickIncrement}>
+
+        <button type="button" className="load-reviews-btn" onClick={this.handleClickIncrement}>
           Load more reviews
         </button>
-        <button type="button" onClick={() => this.setState({ modalOpen: true })}>
+        <button type="button" className="write-review-btn" onClick={() => this.setState({ modalOpen: true })}>
           Write a review
         </button>
         <WriteReview open={this.state.modalOpen} closeModal={() => this.setState({ modalOpen: false })}>
-          <h4 className>Write Your Review</h4>
-          <h6>[Product Name Here]</h6>
-          <div>star rating</div>
-          <div>
+          <div className="write-review-title">Write Your Review</div>
+          <div className="write-review-productname">[Product Name Here]</div>
+          <div className="write-rev-star-cont">star rating {this.getStarsFeature}</div>
+          <div className="write-rev-recommend">
             Do you recommend this product?
             {' '}
-            <button type="button">Yes</button> <button type="button">No</button>
+            <button type="button" className="rec-yes-btn" onClick={() => this.handleChangeRecommend(true)}>
+              Yes
+            </button>
+            <button type="button" className="rec-no-btn" onClick={() => this.handleChangeRecommend(false)}>
+              No
+            </button>
           </div>
-          <div>Characteristics</div>
+          <div className="write-rev-charac-cont">
+            <div className="write-rev-charac-title">Characteristics</div>
+            <div className="size-cont">
+              <div className="write-rev-charac-text">Size</div>
+              <div className="radio-btn-cont">
+                <label className="prod-charac-score">1
+                  <input className="radio-btn" type="radio" id="1" name="score" value="1"></input>
+                </label>
+                <label className="prod-charac-score">2
+                  <input className="radio-btn" type="radio" id="2" name="score" value="2"></input>
+                </label>
+                <label className="prod-charac-score">3
+                  <input className="radio-btn" type="radio" id="3" name="score" value="3"></input>
+                </label>
+                <label className="prod-charac-score">4
+                  <input className="radio-btn" type="radio" id="4" name="score" value="4"></input>
+                </label>
+                <label className="prod-charac-score">5
+                  <input className="radio-btn" type="radio" id="5" name="score" value="5"></input>
+                </label>
+              </div>
+            </div>
+            <div className="width-cont">
+              <div className="write-rev-charac-text">Width</div>
+              <div className="radio-btn-cont">
+                <label className="prod-charac-score">1
+                  <input className="radio-btn" type="radio" id="1" name="score" value="1"></input>
+                </label>
+                <label className="prod-charac-score">2
+                  <input className="radio-btn" type="radio" id="2" name="score" value="2"></input>
+                </label>
+                <label className="prod-charac-score">3
+                  <input className="radio-btn" type="radio" id="3" name="score" value="3"></input>
+                </label>
+                <label className="prod-charac-score">4
+                  <input className="radio-btn" type="radio" id="4" name="score" value="4"></input>
+                </label>
+                <label className="prod-charac-score">5
+                  <input className="radio-btn" type="radio" id="5" name="score" value="5"></input>
+                </label>
+              </div>
+            </div>
+            <div className="comfort-cont">
+              <div className="write-rev-charac-text">Comfort</div>
+              <div className="radio-btn-cont">
+                <label className="prod-charac-score">1
+                  <input className="radio-btn" type="radio" id="1" name="score" value="1"></input>
+                </label>
+                <label className="prod-charac-score">2
+                  <input className="radio-btn" type="radio" id="2" name="score" value="2"></input>
+                </label>
+                <label className="prod-charac-score">3
+                  <input className="radio-btn" type="radio" id="3" name="score" value="3"></input>
+                </label>
+                <label className="prod-charac-score">4
+                  <input className="radio-btn" type="radio" id="4" name="score" value="4"></input>
+                </label>
+                <label className="prod-charac-score">5
+                  <input className="radio-btn" type="radio" id="5" name="score" value="5"></input>
+                </label>
+              </div>
+            </div>
+            <div className="quality-cont">
+              <div className="write-rev-charac-text">Quality</div>
+              <div className="radio-btn-cont">
+                <label className="prod-charac-score">1
+                  <input className="radio-btn" type="radio" id="1" name="score" value="1"></input>
+                </label>
+                <label className="prod-charac-score">2
+                  <input className="radio-btn" type="radio" id="2" name="score" value="2"></input>
+                </label>
+                <label className="prod-charac-score">3
+                  <input className="radio-btn" type="radio" id="3" name="score" value="3"></input>
+                </label>
+                <label className="prod-charac-score">4
+                  <input className="radio-btn" type="radio" id="4" name="score" value="4"></input>
+                </label>
+                <label className="prod-charac-score">5
+                  <input className="radio-btn" type="radio" id="5" name="score" value="5"></input>
+                </label>
+              </div>
+            </div>
+            <div className="length-cont">
+              <div className="write-rev-charac-text">Length</div>
+              <div className="radio-btn-cont">
+                <label className="prod-charac-score">1
+                  <input className="radio-btn" type="radio" id="1" name="score" value="1"></input>
+                </label>
+                <label className="prod-charac-score">2
+                  <input className="radio-btn" type="radio" id="2" name="score" value="2"></input>
+                </label>
+                <label className="prod-charac-score">3
+                  <input className="radio-btn" type="radio" id="3" name="score" value="3"></input>
+                </label>
+                <label className="prod-charac-score">4
+                  <input className="radio-btn" type="radio" id="4" name="score" value="4"></input>
+                </label>
+                <label className="prod-charac-score">5
+                  <input className="radio-btn" type="radio" id="5" name="score" value="5"></input>
+                </label>
+              </div>
+            </div>
+            <div className="fit-cont">
+              <div className="write-rev-charac-text">Fit</div>
+              <div className="radio-btn-cont">
+                <label className="prod-charac-score">1
+                  <input className="radio-btn" type="radio" id="1" name="score" value="1"></input>
+                </label>
+                <label className="prod-charac-score">2
+                  <input className="radio-btn" type="radio" id="2" name="score" value="2"></input>
+                </label>
+                <label className="prod-charac-score">3
+                  <input className="radio-btn" type="radio" id="3" name="score" value="3"></input>
+                </label>
+                <label className="prod-charac-score">4
+                  <input className="radio-btn" type="radio" id="4" name="score" value="4"></input>
+                </label>
+                <label className="prod-charac-score">5
+                  <input className="radio-btn" type="radio" id="5" name="score" value="5"></input>
+                </label>
+              </div>
+            </div>
+          </div>
           <div>
             <input
               className="review-sum-box" placeholder="Example: Best purchase ever!" onChange={this.handleChangeReviewSum}>
